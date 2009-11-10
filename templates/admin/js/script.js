b946be9677999f40ce2f3978c522250ebfd898c5
admin <admin@gplld.com>
2009-11-10 16:56:20 +0000

    Fixed bugs in the last update.

--- templates/admin/js/script.js
+++ templates/admin/js/script.js
@@ -2,8 +2,8 @@
    // do stuff when DOM is ready
    $('.pop').hover(function(){
    	// mouseover
-	$(this).parent().children('pop-list').css("display", "block");
+	$(this).parent().children('.pop-list').css("display", "block");
    }, function(){
-    $(this).parent().children('pop-list').css("display", "none");
+    $(this).parent().children('.pop-list').css("display", "none");
    })
  });
