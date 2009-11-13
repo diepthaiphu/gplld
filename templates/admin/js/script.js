3129cf72a3e3805fb943707e4b3054afd83099f1
admin <admin@gplld.com>
2009-11-13 15:59:04 +0000

    Added javascript effects to link details in the admin panel.

--- templates/admin/js/script.js
+++ templates/admin/js/script.js
@@ -3,14 +3,18 @@ $(document).ready(function() {
 $('.pop').each(function() {
 	var pop = $(this);
 	var popList = $(this).parent().children('.pop-list');
-	$(this).hover(function(){
-		popList.css("display", "block");
-	}, function(){})
-	popList.hover(function(){}, function(){
-		popList.css("display", "none");
-	})
+	pop.hover(function(){popList.show();}, function(){})
+	popList.hover(function(){}, function(){popList.hide();})
 }
 );
+
+$('a.htt').each(function()
+{
+	var htt = $(this);
+	var tt = htt.siblings('.tt');
+	htt.hover(function(){ tt.show();}, function(){});
+	tt.hover(function(){}, function(){tt.hide()});
+});
 
 $('form').jqTransform();
 
